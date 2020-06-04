e6b1c6389ad18bdb904d077e78db97b604a1135f
chernandezdia <chernandezdia@uoc.edu.es>
2020-06-04 23:43:08 +0100

    Quitar videos de páginas

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+const name = 'world';
+console.log(`Hello ${name}`);
+
+
+function validateForm() {
+    var name = document.getElementById('name').value;
+    if (name == "") {
+        document.querySelector('.status').innerHTML = "El nombre no puede estar vacío";
+        return false;
+    }
+    var email = document.getElementById('email').value;
+    if (email == "") {
+        document.querySelector('.status').innerHTML = "El email no puede estar vacío";
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(email)) {
+            document.querySelector('.status').innerHTML = "Formato de email invalido";
+            return false;
+        }
+    }
+    var subject = document.getElementById('subject').value;
+    if (subject == "") {
+        document.querySelector('.status').innerHTML = "El asunto no puede estar vacío";
+        return false;
+    }
+    var message = document.getElementById('message').value;
+    if (message == "") {
+        document.querySelector('.status').innerHTML = "El mensaje no puede estar vacío";
+        return false;
+    }
+    document.querySelector('.status').innerHTML = "Enviando...";
+}
+
+document.getElementById("send").addEventListener("click", validateForm, false);
